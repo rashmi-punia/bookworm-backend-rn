2458ec9f200a0dd1eb52aba5d6e819f644aa98da
rashmi-punia <rashmipunia909@gmail.com>
2025-03-30 15:32:26 +0530

    new error fixed

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -39,6 +39,7 @@ router.get("/", protectRoute, async (req, res) => {
   try {
     const page = req.query.page || 1;
     const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
 
     const books = await Book.find()
       .sort({ createdAt: -1 }) //desc
